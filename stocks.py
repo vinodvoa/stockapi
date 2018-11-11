6a262910cad8098e3b11f8a8511e9ad48b77da43
vinodvoa <vinodvoa@yahoo.com.sg>
2018-11-11 15:42:39 +0800

    Added India UT update

--- stocks.py
+++ stocks.py
@@ -8,22 +8,28 @@ import shutil
 
 import openpyxl as pyxl
 import urllib.request
+import requests
 import json
 import time
-import datetime
 
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
 # ALPHAKEY = 'D9ZW4UF3RPMXif7H'
 QUANDLKEY = 'ahzB_xyURSjA4V7iHXtT'
+# MASHAPEKEY = 'OhAQqUguNXmshsiWsbcrJGpk4UI9p1uAoNsjsnjQcjKDPPJvlx'
 USSHEET = 'US Stocks'
 INSHEET = 'IN Stocks'
 CRYPTOSHEET = 'Crypto'
 GOLDSHEET = 'Gold'
+INDIAUTSHEET = 'IN-UT'
+
 SOURCE = '/Volumes/Secomba/vinodverghese/Boxcryptor/Dropbox/Personal/Finance/Financial statement Master.xlsx'
 BKUPDIR = '/Volumes/Secomba/vinodverghese/Boxcryptor/Dropbox/Personal/Finance/Backup'
 TARGET = '/Users/vinodverghese/Dropbox/Python/Learning/Completed/Stockapi/stockapi/Financial statement Master.xlsx'
-URL = 'https://www.moneymetals.com/precious-metals-charts/gold-price'
+
+GOLDURL = 'https://www.moneymetals.com/precious-metals-charts/gold-price'
+INMFURL = 'https://www.amfiindia.com/spages/NAVAll.txt'
 
 # check if source file exists
 if not (os.path.exists(SOURCE)):
@@ -53,10 +59,11 @@ ext = os.path.splitext(filename)[1]
 # print(ext)
 
 # Get todays date and time
-now = datetime.datetime.now()
+now = datetime.now()
 
 # format date as dd-mm-yyyy
 dte = str(now.day) + '-' + str(now.month) + '-' + str(now.year)
+print('Todays date: ', dte)
 
 # build up backup path
 bkupfilename = filewithoutext + ' ' + dte + ext
@@ -90,6 +97,21 @@ except Exception as e:
 
 print('Backup complete')
 
+# Check if weekend
+if now.weekday() in range(0, 5):  # Weekdays
+    querydte = str(now.year) + '-' + str(now.month) + '-' + str(now.day)
+    print('Query date : ', querydte)
+else:
+    if now.weekday() == 5:  # Sat
+        now = datetime.now() - timedelta(1)
+        querydte = str(now.year) + '-' + str(now.month) + '-' + str(now.day)
+        print('Query date : ', querydte)
+    else:
+        if now.weekday() == 6:  # Sun
+            now = datetime.now() - timedelta(2)
+            querydte = str(now.year) + '-' + str(now.month) + '-' + str(now.day)
+            print('Query date : ', querydte)
+
 # load workbook
 print('Loading workbook')
 
@@ -121,7 +143,8 @@ while (ws.cell(row, 2).value):
 
     quandlurl = 'https://www.quandl.com/api/v3/datasets/NSE/' + \
         indiaticker + \
-        '.json?start_date=2018-10-26&end_date=2018-10-26&api_key=' + QUANDLKEY
+        '.json?start_date=' + querydte + '&end_date=' + querydte + \
+        '&api_key=' + QUANDLKEY
     # print(quandlurl)
 
     # open url
@@ -162,6 +185,73 @@ while (ws.cell(row, 2).value):
     # print(row)
     row += 1
     time.sleep(0.5)
+
+###################################
+print('Getting Indian UT prices..')
+###################################
+ws = wb[INDIAUTSHEET]
+
+# get ticker, call API, update price
+row = 2
+openErr = False
+readErr = False
+parseErr = False
+
+while (ws.cell(row, 2).value):
+    # build API url
+    indiautticker = ws.cell(row, 2).value
+    print(indiautticker)
+
+    quandlurl = 'https://www.quandl.com/api/v3/datasets/AMFI/' + \
+        str(indiautticker) + \
+        '.json?start_date=' + querydte + '&end_date=' + querydte + \
+        '&api_key=' + QUANDLKEY
+    # quandlurl = 'https://www.quandl.com/api/v3/datasets/AMFI/' + \
+    #     str(indiautticker) + \
+    #     '?api_key=' + QUANDLKEY
+
+    # print(quandlurl)
+
+    # open url
+    try:
+        f = urllib.request.urlopen(quandlurl)
+
+    except Exception as e:
+        print('1. URL Open Error')
+        print(e)
+        openErr = True
+
+    # read from url
+    if not openErr:
+        try:
+            stockjson = f.read()
+
+        except Exception as e:
+            print('1. URL Read Error')
+            print(e)
+            readErr = True
+
+    # parse json into dictionary
+    if not openErr and not readErr:
+        try:
+            parsed_json = json.loads(stockjson)
+            # print(parsed_json)
+
+        except Exception as e:
+            print('1. Parsing Error')
+            print(e)
+            parseErr = True
+
+    # print(json.dumps(parsed_json, indent=4, sort_keys=True))
+    # get price from dictionary
+    if not openErr and not readErr and not parseErr:
+        ws.cell(row, 13).value = float(parsed_json['dataset']['data'][0][1])
+        print(ws.cell(row, 13).value)
+
+    # print(row)
+    row += 1
+    time.sleep(0.5)
+
 
 # Process US sheet
 print('Getting US stock prices..')
@@ -295,7 +385,7 @@ print('Getting Gold price..')
 ws = wb[GOLDSHEET]
 
 # load page from url
-page = requests.get(URL)
+page = requests.get(GOLDURL)
 
 # create BS
 soup = BeautifulSoup(page.content)
@@ -310,7 +400,8 @@ price = panel.text.replace('$', '').replace(',', '')
 print(price)
 
 # update price in sheet
-ws['D8'] = float(price)
+ws['D8'].value = float(price)
+print(ws['D8'].value)
 
 # save workbook
 print('Saving workbook..')
